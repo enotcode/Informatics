fd2c1a02083e02cb28ee381c1fb549bb45a82d2b
Viktor <enotcode@gmail.com>
2017-09-08 15:39:50 +0300

    Edit

--- HW1/first.cpp
+++ HW1/first.cpp
@@ -2,10 +2,6 @@
 using namespace std;
 
 int main() { 
-    cout << "Viktor, Nizhniy Novogorod" << endl;
-    cout << "Rasstoyanie: ";
-    int r;
-    cin >> r;
-    cout << "Yardy:" << r*220 << endl;
+    cout << "Viktor, Nizhny Novgorod" << endl;
     return 0; 
 }
---
+++ HW1/furlong.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() { 
+	int r;
+    cout << "Furlong: ";
+    cin >> r;
+    cout << "Yard:" << r*220 << endl;
+
+    return 0; 
+}

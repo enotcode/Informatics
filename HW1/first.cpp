c1152476a5bb96f4d6855149e68bc8fbc9f01118
Viktor <enotcode@gmail.com>
2017-09-08 14:23:29 +0300

    HomeWork

---
+++ HW1/first.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() { 
+    cout << "Viktor, Nizhniy Novogorod" << endl;
+    cout << "Rasstoyanie: ";
+    int r;
+    cin >> r;
+    cout << "Yardy:" << r*220 << endl;
+    return 0; 
+}

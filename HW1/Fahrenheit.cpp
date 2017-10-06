a64a554fcc9775c6ab8b63e2ffe4cfd2493a18ab
Viktor <enotcode@gmail.com>
2017-10-06 14:14:52 +0300

    fix🔥

--- HW1/Fahrenheit.cpp
+++ HW1/Fahrenheit.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int main() { 
-	int c;
-	double f;
+double converter(double c){
+	return 1.8*c + 32.0;
+}
+int main() {
+	double c;
 	cout << "Please enter a Celsius value: ";
 	cin >> c;
-	f = 1.8*c + 32.0;
-	cout << c << " degrees Celsius is " << f << " degrees Fahrenheit" << endl;
-    return 0; 
+	cout << c << " degrees Celsius is " << converter(c) << " degrees Fahrenheit" << endl;
+    return 0;
 }
